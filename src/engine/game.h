b28a07d10dc971b63353069599af4a2c4f9036a3
thibaud-heub <121852303+thibaud-heub@users.noreply.github.com>
2023-01-09 09:14:31 +0100

    Add files via upload

---
+++ src/engine/game.h
@@ -0,0 +1,276 @@
+#ifndef _GAME_H
+#define _GAME_H
+
+#include "game_types.h" 
+ 
+void initGame(board, unitsArray); //initialize the board, put obstacles and neutral units (symetric)
+
+void printBoard(board plateau); //print the board on terminal
+
+void wait(void);
+
+void print_unitsArray(unitsArray nb_unite); //print the unitsArray
+
+void move(board, unitsArray, int unitId, int x, int y);
+
+void shoot(board plateau, unitsArray nb_unite, int uniteId, int targetId);
+
+void convert(board, unitsArray, int unitId, int targetId);
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*int algo_Bresenham(board board, int x1, int y1, int x2, int y2,tab_shoot tableau_mouv)
+{
+    int dx,dy,compt=0;
+    if((dx=x2-x1)!=0)
+    {
+        if(dx>0)
+        {
+            if((dy=y2-y1)!=0)
+            {
+                if(dy>0)
+                {
+                    if(dx>=dy)
+                    {
+                        int e;
+                        e=dx;
+                        dx=e*2;
+                        dy=dy*2;
+                        while(1)
+                        {
+                            tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                            tableau_mouv[compt][1]=y1;
+                            compt++;
+                            if((x1=x1+1)==x2) break;
+                            if((e=e-dy)<0)
+                            {
+                                y1++;
+                                e=e+dx;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int e;
+                        e=dy;
+                        dy=e*2;
+                        dx=dx*2;
+                        while(1)
+                        {
+                            tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                            tableau_mouv[compt][1]=y1;
+                            compt++;
+                            if((y1++)==y2) break;
+                            if((e=e-dx)<0)
+                            {
+                                x1++;
+                                e=e+dy;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if(dx>=-dy)
+                    {
+                        int e;
+                        e=dx;
+                        dx=e*2;
+                        dy=dy*2;
+                        while(1)
+                        {
+                            tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                            tableau_mouv[compt][1]=y1;
+                            compt++;
+                            if((x1++)==x2) break;
+                            if((e=e+dy)<0)
+                            {
+                                y1++;
+                                e=e+dx;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int e;
+                        e=dy;
+                        dy=e*2;
+                        dx=dx*2;
+                        while(1)
+                        {
+                            tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                            tableau_mouv[compt][1]=y1;
+                            compt++;
+                            if((y1--)==y2) break;
+                            if((e=e+dx)>0)
+                            {
+                                x1++;
+                                e=e+dy;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                do
+                {
+                    tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                    tableau_mouv[compt][1]=y1;
+                    compt++;
+                } while ((x1++)!=x2);
+            }
+        }
+        else
+        {
+            if((dy=y2-y1)!=0)
+            {
+                if(dy>0)
+                {
+                    if(-dx>=dy)
+                    {
+                        int e;
+                        e=dx;
+                        dx=e*2;
+                        dy=dy*2;
+                        while(1)
+                        {
+                            tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                            tableau_mouv[compt][1]=y1;
+                            compt++;
+                            if((x1--)==x2) break;
+                            if((e=e+dy)<0)
+                            {
+                                y1++;
+                                e=e+dx;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int e;
+                        e=dy;
+                        dy=e*2;
+                        dx=dx*2;
+                        while(1)
+                        {
+                            tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                            tableau_mouv[compt][1]=y1;
+                            compt++;
+                            if((y1++)==y2) break;
+                            if((e=e+dx)>0)
+                            {
+                                x1--;
+                                e=e+dy;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if(dx<=dy)
+                    {
+                        int e;
+                        e=dx;
+                        dx=e*2;
+                        dy=dy*2;
+                        while(1)
+                        {
+                            tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                            tableau_mouv[compt][1]=y1;
+                            compt++;
+                            if((x1--)==x2) break;
+                            if((e=e-dy)<0)
+                            {
+                                y1--;
+                                e=e+dx;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int e;
+                        e=dy;
+                        dy=e*2;
+                        dx=dx*2;
+                        while(1)
+                        {
+                            tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                            tableau_mouv[compt][1]=y1;
+                            compt++;
+                            if((y1--)==y2) break;
+                            if((e=e-dx)>0)
+                            {
+                                x1--;
+                                e=e+dy;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                do
+                {
+                    tableau_mouv[compt][0]=x1;//board[x1][y1]='T';
+                    tableau_mouv[compt][1]=y1;
+                    compt++;
+                } while ((x1--)!=x2);
+            }
+        }
+        
+    }
+    else
+    {
+        if((dy=y2-y1)!=0)
+        {
+            if(dy>0)
+            {
+                do
+                {
+                    tableau_mouv[compt][0]=x1;
+                    tableau_mouv[compt][1]=y1;
+                    //board[x1][y1]='T';
+                    compt++;
+                } while ((y1++)!=y2);
+            }
+            else
+            {
+                do
+                {
+                    tableau_mouv[compt][0]=x1;
+                    tableau_mouv[compt][1]=y1;
+                    //board[x1][y1]='T';
+                    compt++;
+                } while ((y1--)!=y2);   
+            }
+        }
+    }
+     //Pour voir quelles sont les valeurs dans le tableau des coordonnées
+    for(int i=0;i<compt;i++)
+    {
+        printf("%d %d\n",tableau_mouv[i][0],tableau_mouv[i][1]);
+    }
+    return compt;
+}*/
---
+++ src/engine/game_engine.c
@@ -0,0 +1,114 @@
+/**
+ * ----------------------------------------------
+ *                 Game_Engine.c
+ * ----------------------------------------------
+ * 
+ * Moteur du jeu, il fait le lien entre 
+ * toutes les fonctions du fichiers game.h
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include "game.h"
+
+int action(int player,board plateau,unitsArray nb_unite)
+{
+    int choix,conv,target_conv,avanc,x,y,tire,target_tire,turn = 0;
+    printf("Joueur %d, à vous de jouer.\n",player);
+    printf("Vous avez le choix de faire :\n");
+    printf("- 0 : Convertir une unité \n");
+    printf("- 1 : Faire avancer une unité \n");
+    printf("- 2 : Tirer sur une unité \n");
+    printf("- 3 : Attendre \n");
+    scanf("%d",&choix);
+    switch(choix)
+    {
+        case 0 :
+            printf("Quelle unité va convertir ?");
+            scanf("%d",&conv);
+            printf("Quelle est l'unité ciblée ?");
+            scanf("%d",&target_conv);
+            convert(plateau,nb_unite,conv,target_conv);
+            turn++;
+            break;
+        case 1:
+            printf("Quelle unité va avancer ?");
+            scanf("%d",&avanc);
+            printf("Quelle sont les coordonnées de la case ciblée(libre) ?");
+            scanf("%d %d",&x,&y);
+            move(plateau,nb_unite,avanc,x,y);
+            turn++;
+            break;
+        case 2:
+            printf("Quelle unité va tirer ?");
+            scanf("%d",&tire);
+            printf("Quelle est l'unité ciblée ?");
+            scanf("%d",&target_tire);
+            shoot(plateau,nb_unite,tire,target_tire);
+            turn++;
+            break;
+        default:
+            wait();
+            turn++;
+            break;
+
+    }
+    sleep(3);
+    system("clear");
+    print_unitsArray(nb_unite);
+    printBoard(plateau);
+    end_of_game(plateau,nb_unite,player,turn);
+    return choix;
+}
+int end_of_game(board plateau,unitsArray nb_unite, int player,int turn)
+{
+    int i,j,nb_j0,nb_j1;
+    for(i=1;i<=MAX_UNITS;i++)
+    {
+        if(nb_unite->units[i].owner != 0)
+        {
+            return 0;
+        }
+    }
+    if(turn == MAX_TURNS)
+    {
+        for(j=1;j<=MAX_UNITS;j++)
+        {
+            if(nb_unite->units[j].owner == 0) nb_j0++;
+            else if(nb_unite->units[j].owner == 1) nb_j1++;
+        }
+        if(nb_j0 > nb_j1 && player == 0) 
+        {
+            return 1;
+        }
+        else
+        {
+            if(nb_j0 < nb_j1 && player == 1) 
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+    return 1;// Le jeu est fini et renvoie 1
+}
+int main(int argc, char const *argv[])
+{ 
+    //int fin = 1;
+    board plateau;
+    unitsArray nb_unite = malloc(sizeof(struct unitsArray_s));
+    initGame(plateau,nb_unite);
+    print_unitsArray(nb_unite);
+    printBoard(plateau);
+    move(plateau,nb_unite,2,6,4);
+    printBoard(plateau);
+    print_unitsArray(nb_unite);
+    /*while(fin)
+    {
+        action(0,plateau,nb_unite);
+        action(1,plateau,nb_unite);
+    }*/
+    return 0;
+}
